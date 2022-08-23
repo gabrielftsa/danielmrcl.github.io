ba9ac73d8fda1ae36733ffcebf3266a6a0d066a6
Gabriel Feitosa <70492667+gabrielftsa@users.noreply.github.com>
2022-08-23 17:44:36 -0300

    adiciona-detalhes

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ function carregar() {
     var dataNow = new Date
     var birthDate = new Date("2004-03-05")
     var dateTime = (dataNow - birthDate) / 1000 / 60 / 60 / 24 / 365
-    detailsElement.innerHTML = `Gabriel Feitosa / ${Math.floor(dateTime)}anos / Brazilian / Dev`
+    detailsElement.innerHTML = `Gabriel Feitosa / ${Math.floor(dateTime)}y / Brazilian / Dev`
 }
